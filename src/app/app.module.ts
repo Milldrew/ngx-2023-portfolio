6ff462bc1b9a14d582e0c6834c551b921035dccc
Andrew Miller <andrew.patrick.miller.2022@gmail.com>
2023-03-23 17:40:32 -0600

    📅  | Thu Mar 23 17:40:32 MDT 2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutMeComponent } from './sections/about-me/about-me.component';
+import { ExperienceComponent } from './sections/experience/experience.component';
+import { ProjectsComponent } from './sections/projects/projects.component';
+import { ContactMeComponent } from './sections/contact-me/contact-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    ContactMeComponent
   ],
   imports: [
     BrowserModule,
