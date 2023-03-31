2eaf9ad4db74b6b206960630173ef211d293e2e1
Andrew Miller <andrew.patrick.miller.2022@gmail.com>
2023-03-30 19:55:14 -0600

    📅  | Thu Mar 30 19:55:14 MDT 2023

---
+++ src/app/tabs/projects/projects-table/projects-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'frisky-projects-table',
+  templateUrl: './projects-table.component.html',
+  styleUrls: ['./projects-table.component.scss']
+})
+export class ProjectsTableComponent {
+
+}
