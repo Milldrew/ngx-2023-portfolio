fff86d6558fc818b4371ba7c1ae53f9fb95e45a0
Andrew Miller <andrew.patrick.miller.2022@gmail.com>
2023-03-30 18:37:29 -0600

    📅  | Thu Mar 30 18:37:29 MDT 2023

--- src/app/chart-area/chart-area.component.ts
+++ src/app/chart-area/chart-area.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'frisky-chart-area',
   templateUrl: './chart-area.component.html',
-  styleUrls: ['./chart-area.component.scss']
+  styleUrls: ['./chart-area.component.scss'],
 })
-export class ChartAreaComponent {
-
-}
+export class ChartAreaComponent {}
