c07efb14cdec32a3c48c8c56387ef6fdaf96688d
Andrew Miller <andrew.patrick.miller.2022@gmail.com>
2023-03-30 19:15:18 -0600

    📅  | Thu Mar 30 19:15:18 MDT 2023

---
+++ github-client.sh
@@ -0,0 +1,7 @@
+#echo $GH_PORTFOLIO
+# curl -H "Authorization: Bearer "$GH_PORTFOLIO https://api.github.com/milldrew/repos
+#curl -H  https://api.github.com/users/milldrew/repos
+
+curl --request GET \
+--url "https://api.github.com/users/milldrew/repos" \
+--header "Authorization: Bearer "$GH_PORTFOLIO  > payload.json
