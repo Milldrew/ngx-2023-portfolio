02de0190a252e81476856c6e54f6a7a1d66b9e9f
Andrew Miller <andrew.patrick.miller.2022@gmail.com>
2023-03-30 20:01:56 -0600

    📅  | Thu Mar 30 20:01:56 MDT 2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
@@ -6,26 +7,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AboutMeComponent } from './sections/about-me/about-me.component';
-import { ExperienceComponent } from './sections/experience/experience.component';
-import { ProjectsComponent } from './sections/projects/projects.component';
-import { ContactMeComponent } from './sections/contact-me/contact-me.component';
 import { AppBarComponent } from './app-bar/app-bar.component';
 import { ChartAreaComponent } from './chart-area/chart-area.component';
 import { TabsComponent } from './tabs/tabs.component';
+import { ProjectsTableComponent } from './tabs/projects/projects-table/projects-table.component';
+import { ProjectsComponent } from './tabs/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutMeComponent,
-    ExperienceComponent,
-    ProjectsComponent,
-    ContactMeComponent,
     AppBarComponent,
     ChartAreaComponent,
     TabsComponent,
+    ProjectsComponent,
+    ProjectsTableComponent,
   ],
   imports: [
+    MatTableModule,
     MatTabsModule,
     MatToolbarModule,
     BrowserModule,
--- src/app/tabs/projects/projects-table/projects-table.component.ts
+++ src/app/tabs/projects/projects-table/projects-table.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { GithubService } from 'src/app/services/github.service';
 
 @Component({
   selector: 'frisky-projects-table',
   templateUrl: './projects-table.component.html',
-  styleUrls: ['./projects-table.component.scss']
+  styleUrls: ['./projects-table.component.scss'],
 })
 export class ProjectsTableComponent {
-
+  constructor(public githubService: GithubService) {}
 }
